937f52bbe88846123138a4c2a377076c76b75800
Pedro Godoi <pedrogodoiriva@gmail.com>
2026-02-20 09:01:35 -0300

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="h-screen flex flex-col items-center justify-center">
+    <h1 class="title">Welcome to Nuxt 3!</h1>
+    <p class="description">
+      Get started by editing <code>app/pages/index.vue</code>
+    </p>
+  </div>
+</template>
